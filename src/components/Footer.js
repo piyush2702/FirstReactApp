c584498b9ce7598882f58477ed2c5863db54f4f1
piyush2702 <53112560+piyush2702@users.noreply.github.com>
2020-01-12 21:50:22 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from "React";
+const Footer = () => {
+  return <div />;
+};
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "React";
+const Header = () => {
+  return <div />;
+};
+export default Header;
